b5c7b54af4812435f38396fa2a4761a8c3344c03
leydisa <l.alfonso@protelcotelsa.com>
2019-08-14 11:43:54 -0500

    [ADD] Add module maintenance_control_system.

---
+++ maintenance_control_system/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Maintenance Control System',
+    'version': '1.0',
+    'summary': 'Maintenance Control System',
+    'description': "",
+    'website': 'https://www.example.com',
+    'depends': [],
+    'category': 'maintenance_control_system',
+    'sequence': 13,
+    'demo': [
+    ],
+    'data': [
+        'data/data.xml',
+        'views/maintenance_control_system.xml',
+    ],
+    'qweb': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
